aa72e589f3d5458124db6841fd7429bf0883ce84
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-11-09 23:43:13 +0100

    Clean scraped data (#1)

--- nature.py
+++ nature.py
@@ -1,22 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 from typing import List, Optional, Dict, Any
-from dataclasses import dataclass
-
-import time
-
-SUBJECTS = ["physical-sciences", "earth-and-environmental-sciences", "biological-sciences", "health-sciences",
-            "scientific-community-and-society"]
+from dataclasses import dataclass, asdict
+import json
 
 
 @dataclass
 class Article:
-    type: Optional[str]
-    title: Optional[str]
-    short_desc: Optional[str]
-    authors: List[Optional[str]]
-    url: Optional[str]
-    date: Optional[str]
+    type: str
+    title: str
+    short_desc: str
+    authors: List[str]
+    url: str
+    date: str
     is_free: bool = False
 
 
@@ -51,17 +47,22 @@ def get_all_articles(soup) -> Optional[List]:
 
 
 def get_article_data(article_obj) -> Optional[Article]:
+    short_desc = None
+
     type = article_obj.find("span", {"data-test": "article.type"})
     is_free = article_obj.find("span", class_="text-orange")
     date = article_obj.find('time', {"itemprop": "datePublished"})
     title_url = article_obj.find("a", class_="text-gray")
-    short_desc = article_obj.find("p")
+    desc_container = article_obj.find("div", {"itemprop": "description"})
+    if desc_container:
+        short_desc = desc_container.find("p")
     authors_container = article_obj.find("ul", {"data-test": "author-list"})
+
     authors = []
     if authors_container:
         author_objects = authors_container.find_all("li")
         for author in author_objects:
-            authors.append(author.text)
+            authors.append(author.text.replace("\xa0&", "").replace("\xa0", ""))
 
     if not type or not date or not title_url or not short_desc or not authors:
         return None
@@ -72,8 +73,8 @@ def get_article_data(article_obj) -> Optional[Article]:
 
     return Article(
         type=type.text,
-        title=title_url.text,
-        short_desc=short_desc.text,
+        title=title_url.text.strip(),
+        short_desc=short_desc.text.replace("\xa0", " "),
         authors=authors,
         url=f"https://www.nature.com{title_url['href']}",
         is_free=free,
@@ -81,15 +82,22 @@ def get_article_data(article_obj) -> Optional[Article]:
     )
 
 
-if __name__ == "__main__":
-    url = f"https://www.nature.com/subjects/physical-sciences/nature?searchType=journalSearch&sort=PubDate&page=1"
-    content = get_page_content(url)
-    # max_page = get_max_page(content)
-    # print(max_page)
 
-    all_articles = get_all_articles(content)
-    for article in all_articles:
-        article_data = get_article_data(article)
-        if article_data:
-            print(article_data)
-            print("=======================")
+
+if __name__ == "__main__":
+    SUBJECTS = ["physical-sciences", "earth-and-environmental-sciences", "biological-sciences", "health-sciences",
+                "scientific-community-and-society"]
+
+    for subject in SUBJECTS:
+        url = f"https://www.nature.com/subjects/{subject}/nature?searchType=journalSearch&sort=PubDate&page=1"
+        content = get_page_content(url)
+        max_page = get_max_page(content)
+
+        for i in range(1, max_page + 1):
+            url = f"https://www.nature.com/subjects/{subject}/nature?searchType=journalSearch&sort=PubDate&page={i}"
+
+            all_articles = get_all_articles(content)
+            for article in all_articles:
+                article_data = get_article_data(article)
+                if article_data:
+                    asdict(article_data)
