ce7e08141a3ec4af7b3486a93aed31aabc64fa35
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-11-10 16:22:04 +0100

    Create Schemas for Type, Author and Data

--- models.py
+++ models.py
@@ -20,13 +20,13 @@ class Author(Base):
 class Data(Base):
     __tablename__ = "data"
     id = Column(Integer, primary_key=True, autoincrement=True)
-    type_id = Column(Integer, ForeignKey('type.id'), nullable=True)
-    type = relationship('Type', back_populates='data', nullable=True)
     title = Column(String, nullable=False)
     short_desc = Column(Text, nullable=True)
-    authors = relationship('Author', secondary='data_author', back_populates='data', nullable=True)
     url = Column(String, nullable=False)
-    is_free = Column(Boolean, nullable=False)
+    is_free = Column(Boolean, default=False)
     published_date = Column(String, nullable=True)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
     updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now(), default=None)
+    type_id = Column(Integer, ForeignKey('type.id'), nullable=True)
+    type = relationship('Type', back_populates='data', nullable=True)
+    authors = relationship('Author', secondary='data_author', back_populates='data', nullable=True)
---
+++ schemas.py
@@ -0,0 +1,69 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class TypeSchema(BaseModel):
+    id: int
+    name: str
+
+    class Config:
+        orm_mode = True
+
+
+class TypeCreateSchema(BaseModel):
+    name: str
+
+
+class TypeRequestSchema(BaseModel):
+    id: int
+
+
+class ListTypeSchema(BaseModel):
+    types: List[Optional[TypeSchema]]
+
+
+class AuthorSchema(BaseModel):
+    id: int
+    full_name: str
+
+    class Config:
+        orm_mode = True
+
+
+class AuthorRequestSchema(BaseModel):
+    id: int
+
+
+class AuthorCreateSchema(BaseModel):
+    full_name: str
+
+
+class ListAuthorSchema(BaseModel):
+    authors: List[Optional[AuthorSchema]]
+
+
+class DataSchema(BaseModel):
+    id: int
+    title: str
+    short_desc: Optional[str]
+    url: str
+    is_free: bool = False
+    published_date: Optional[str]
+    type: Optional[TypeSchema]
+    authors: Optional[List[AuthorSchema]]
+
+    class Config:
+        orm_mode = True
+
+
+class ListDataSchema(BaseModel):
+    data: List[Optional[DataSchema]]
+
+
+class CreateDataSchema(BaseModel):
+    title: str
+    short_desc: Optional[str]
+    url: str
+    published_date: Optional[str]
+    type_id: Optional[int]
+    authors: Optional[List[int]]
