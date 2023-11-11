62ff428672e9cd914ffa35d5d27b9324b8ab29ed
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-11-11 01:48:04 +0100

    Add new methods for Type repository

--- src/repository/type.py
+++ src/repository/type.py
@@ -1,10 +1,18 @@
 from ..schemas.type import TypeCreateSchema, TypeUpdateSchema
 from ..models import Type
 from ..repository.base import RepositoryBase
+from sqlalchemy.orm import Session
 
 
 class RepositoryType(RepositoryBase[Type, TypeCreateSchema, TypeUpdateSchema]):
-    pass
+    def __ini__(self):
+        super().__init__(Type)
+
+    def type_object_exists(self, db: Session, name: str) -> bool:
+        return db.query(Type).filter(Type.name == name).first() is not None
+
+    def get_by_name(self, db: Session, name: str) -> Type:
+        return db.query(Type).filter(Type.name == name).first()
 
 
 types = RepositoryType(Type)
