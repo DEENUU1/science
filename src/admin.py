72d1541e248a5e61999a134f4f794a622d8cb932
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-11-11 01:21:50 +0100

    Add configuration for admin view

--- src/admin.py
+++ src/admin.py
@@ -4,11 +4,23 @@ from .models import Type, Data, Author
 
 class TypeAdmin(ModelView, model=Type):
     column_list = [Type.id, Type.name]
+    column_searchable_list = [Type.name]
+    name_plural = "Types"
+    page_size = 50
+    page_size_options = [25, 50, 100, 200]
 
 
 class DataAdmin(ModelView, model=Data):
     column_list = [Data.id, Data.title, Data.type, Data.authors, Data.is_free, Data.published_date]
+    column_searchable_list = [Data.title, Data.type, Data.authors, Data.is_free]
+    name_plural = "Datas"
+    page_size = 50
+    page_size_options = [25, 50, 100, 200]
 
 
 class AuthorAdmin(ModelView, model=Author):
     column_list = [Author.id, Author.full_name]
+    column_searchable_list = [Author.full_name]
+    name_plural = "Authors"
+    page_size = 50
+    page_size_options = [25, 50, 100, 200]
