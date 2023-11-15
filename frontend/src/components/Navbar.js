baa50a8af49db749393b752884e1e9a63db01d13
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-11-15 15:00:00 +0100

    Implement demo of a navigation bar

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">Science</a>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+export default Navbar;
