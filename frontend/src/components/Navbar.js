7648ca514212f9761874e788ec7f118ed4f7b27a
DEENUU1 <kacperwlodarczy@protonmail.com>
2023-11-15 15:05:03 +0100

    Add logout in nextjs (#11)

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,22 @@
+'use client'
+
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsLoggedIn(false);
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -11,9 +27,15 @@ const Navbar = () => {
           <li>
             <Link href="/">Home</Link>
           </li>
+          {isLoggedIn && (
+            <li>
+              <Link href="#" onClick={handleLogout}>Logout</Link>
+            </li>
+          )}
         </ul>
       </div>
     </div>
   );
 };
+
 export default Navbar;
